125cad1f71f5ad52b3237914e1964344a1b3d654
M K <64108589+findouter@users.noreply.github.com>
2023-04-09 16:47:48 +0000

    Added main() function, if name = main, and question3 function.

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 # This is an example project for replit and git!
 
+#
+def question3():
+  pass
+
+#takes a number and 
+#Don't forget to convert the string into an integer
+
+
+#tells the user if the number is greater than 10,
+
+
+#less than 10, 
+
+
+#or equal to 10. 
+
+
+
+def main():
+  question3()
+
+
+if __name__ == "__main__":
+  main()
